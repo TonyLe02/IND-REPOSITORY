fb1da9e1b167c575d7de818ce4c727f48d0a5689
Tony <tonynl@student.uia.no>
2023-03-07 11:39:44 +0100

    Create modul6.java

---
+++ test/modul6.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class MusicOrganizer {
+    private ArrayList<String> files;
+
+    // Initierer klassen og feltene
+    public MusicOrganizer() {
+        files = new ArrayList<>();
+    }
+}
