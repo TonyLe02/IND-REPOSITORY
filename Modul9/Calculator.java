2a6315126f40f7d3af8f91969e8c170c0744bb31
TonyLe02 <tonynl@student.uia.no>
2023-03-21 13:24:55 +0100

    ee

--- Modul9/Calculator.java
+++ Modul9/Calculator.java
@@ -1,9 +1,9 @@
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+    public int add(int a, int b) { // Metode for å legge til to tall
+        return a + b; // Returnerer summen av de to tallene
     }
 
-    public int multiply(int a, int b) {
-        return a * b;
+    public int multiply(int a, int b) { // Metode for å multiplisere to tall
+        return a * b; // Returnerer produktet av de to tallene
     }
 }
--- Modul9/CalculatorTest.java
+++ Modul9/CalculatorTest.java
@@ -1,18 +1,18 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
+import static org.junit.Assert.*; // Importerer assert-metodene fra JUnit-biblioteket
+import org.junit.Test; // Importerer Test-annotasjonen fra JUnit-biblioteket
 
 public class CalculatorTest {
     @Test
-    public void testAdd() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(2, 3);
-        assertEquals(5, result);
+    public void testAdd() { // Testmetode for add-metoden i Calculator-klassen
+        Calculator calculator = new Calculator(); // Oppretter en instans av Calculator-klassen
+        int result = calculator.add(2, 3); // Kaller add-metoden med argumentene 2 og 3, og lagrer resultatet i en variabel
+        assertEquals(5, result); // Sjekker at resultatet er lik 5 ved hjelp av assert-metoden assertEquals
     }
 
     @Test
-    public void testMultiply() {
-        Calculator calculator = new Calculator();
-        int result = calculator.multiply(2, 3);
-        assertEquals(6, result);
+    public void testMultiply() { // Testmetode for multiply-metoden i Calculator-klassen
+        Calculator calculator = new Calculator(); // Oppretter en instans av Calculator-klassen
+        int result = calculator.multiply(2, 3); // Kaller multiply-metoden med argumentene 2 og 3, og lagrer resultatet i en variabel
+        assertEquals(6, result); // Sjekker at resultatet er lik 6 ved hjelp av assert-metoden assertEquals
     }
 }
