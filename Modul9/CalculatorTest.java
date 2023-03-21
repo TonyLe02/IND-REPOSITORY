4f65110e8419409bf416997117b4727aba35c04c
TonyLe02 <tonynl@student.uia.no>
2023-03-21 13:07:26 +0100

    push

---
+++ Modul9/CalculatorTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(2, 3);
+        assertEquals(6, result);
+    }
+}
